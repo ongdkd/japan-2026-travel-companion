63f1d6a98c818733141cfb829554d2da41fff7b5
ongdkd <50777180+ongdkd@users.noreply.github.com>
2026-09-07 12:23:59 +0700

    Import Japan 2026 Travel Companion app

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE = 'japan-2026-v8';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './styles.css',
+  './connected.css',
+  './data.js',
+  './live-sync.js',
+  './app.js',
+  './manifest.webmanifest',
+  './app-icon.svg',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('./index.html')))
+  );
+});
